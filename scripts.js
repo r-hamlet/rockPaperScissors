99554678136a07c6c0e380464d137ea85edc7e55
r-hamlet <rthamlet6@gmail.com>
2022-01-09 04:15:35 -0500

    added button

--- scripts.js
+++ scripts.js
@@ -1,53 +1,53 @@
-function computerPlay(){
+function computerPlay() {
     var choice = Math.floor(Math.random() * 3);
     switch (choice) {
-        case 0 : return "rock";
-        break;
-        case 1 : return "paper";
-        break;
-        case 2 : return "scissors";
+        case 0: return "rock";
+            break;
+        case 1: return "paper";
+            break;
+        case 2: return "scissors";
     }
 }
 
-function playRound(computerSelection){
+function playRound(computerSelection) {
     playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
     switch (playerSelection) {
-        case 'rock' : switch (computerSelection) {
-            case 'rock' : return 'tie'
-            break;
-            case 'paper' : return 'lose'
-            break;
-            case 'scissors' : return 'win'
+        case 'rock': switch (computerSelection) {
+            case 'rock': return 'tie'
+                break;
+            case 'paper': return 'lose'
+                break;
+            case 'scissors': return 'win'
         }
-        break;
-        case 'paper' : switch (computerSelection) {
-            case 'rock' : return 'win'
             break;
-            case 'paper' : return 'tie'
-            break;
-            case 'scissors' : return 'lose'
+        case 'paper': switch (computerSelection) {
+            case 'rock': return 'win'
+                break;
+            case 'paper': return 'tie'
+                break;
+            case 'scissors': return 'lose'
         }
-        break;
-        case 'scissors' : switch (computerSelection) {
-            case 'rock' : return 'lose'
             break;
-            case 'paper' : return 'win'
-            break;
-            case 'scissors' : return 'tie'
+        case 'scissors': switch (computerSelection) {
+            case 'rock': return 'lose'
+                break;
+            case 'paper': return 'win'
+                break;
+            case 'scissors': return 'tie'
         }
     }
 }
 
-function game(rounds){
+function game(rounds) {
     playerScore = 0;
     computerScore = 0;
     for (i = 0; i < rounds; i++) {
         switch (playRound(computerPlay())) {
-            case 'win' : playerScore++;
-            break;
-            case 'lose' : computerScore++;
-            break;
-            case 'tie' : console.log("tie");
+            case 'win': playerScore++;
+                break;
+            case 'lose': computerScore++;
+                break;
+            case 'tie': console.log("tie");
         }
     }
     if (playerScore > computerScore) {
